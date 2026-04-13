29b5e4322809f8c7605cd09d87b99d587f00fd09
abhilashdk2016 <abhilashdk2014@outlook.com>
2026-04-13 12:16:19 +0530

    add href to links

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -4,20 +4,63 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat.Room
   alias Slax.Repo
 
-  def mount(_params, _session, socket) do
-    room = Room |> Repo.all() |> List.first()
-    IO.puts(room)
-    {:ok, assign(socket, hide_topic?: false, room: room)}
+  def mount(params, _session, socket) do
+    rooms = Repo.all(Room)
+
+    room =
+      case Map.fetch(params, "id") do
+        {:ok, id} -> Repo.get!(Room, id)
+        :error -> List.first(rooms)
+      end
+
+    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
   def handle_event("toggle-topic", _value, socket) do
     {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href={~p"/rooms/#{@room.id}"}
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        {@room.name}
+      </span>
+    </a>
+    """
+  end
+
   @spec render(any()) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
+      <div class="flex flex-col shrink-0 w-64 bg-slate-100">
+        <div class="flex justify-between items-center shrink-0 h-16 border-b border-slate-300 px-4">
+          <div class="flex flex-col gap-1.5">
+            <h1 class="text-lg font-bold text-gray-800">
+              Slax
+            </h1>
+          </div>
+        </div>
+        <div class="mt-4 overflow-auto">
+          <div class="flex items-center h-8 px-3">
+            <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+          </div>
+          <div id="rooms-list">
+            <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+          </div>
+        </div>
+      </div>
       <div class="flex flex-col grow shadow-lg">
         <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
           <div class="flex flex-col gap-1.5">
