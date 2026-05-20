244a28e7fd35942bf2cf0dcb67bb286050d83db9
abhilashdk2016 <abhilashdk2014@outlook.com>
2026-05-20 20:49:29 +0530

    extracted chat context, added function component & sidebar

---
+++ lib/slax/chat.ex
@@ -0,0 +1,12 @@
+defmodule Slax.ChatContext do
+  alias Slax.Chat.Room
+  alias Slax.Repo
+
+  def list_rooms do
+    Repo.all(Room)
+  end
+
+  def get_room!(id) do
+    Repo.get!(Room, id)
+  end
+end
--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,10 +1,89 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
+  alias Slax.Chat.Room
+  alias Slax.ChatContext
+
+  def mount(_params, _session, socket) do
+    rooms = Chat.list_rooms()
+
+    {:ok, assign(socket, rooms: rooms)}
+  end
+
+  def handle_params(params, _uri, socket) do
+    room =
+      case Map.fetch(params, "id") do
+        {:ok, id} -> Chat.get_room!(id)
+        :error -> List.first(socket.assigns.rooms)
+      end
+
+    {:ok, assign(socket, hide_topic?: false, room: room)}
+  end
+
+  def handle_event("toggle-topic", _params, socket) do
+    {:noreply, assign(socket, :hide_topic?, &(!&1))}
+  end
+
+  attr :active, :boolean, required: true
+  attr :room, :map, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <.link
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      navigate={~p"/rooms/#{@room}"}
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        {@room.name}
+      </span>
+    </.link>
+    """
+  end
+
   def render(assigns) do
     ~H"""
-    <h1>Chat Room</h1>
-    <p>Welcome to the chat room!</p>
+    <Layouts.app flash={@flash}>
+      <div class="flex flex-col shrink-0 w-64 bg-slate-100">
+        <div class="flex justify-between items-center shrink-0 h-16 border-b border-slate-300 px-4">
+          <div class="flex flex-col gap-1.5">
+            <h1 class="text-lg font-bold text-gray-800">
+              Slax
+            </h1>
+          </div>
+        </div>
+        <div class="mt-4 overflow-auto">
+          <div class="flex items-center h-8 px-3">
+            <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+          </div>
+          <div id="rooms-list">
+            <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col grow shadow-lg">
+        <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
+          <div class="flex flex-col gap-1.5">
+            <h1 class="text-sm font-bold leading-none">
+              {@room.name}
+            </h1>
+            <div
+              class={["text-xs leading-none h-3.5", @hide_topic? && "text-slate-600"]}
+              phx-click="toggle-topic"
+            >
+              <%= if @hide_topic? do %>
+                [Topic hidden]
+              <% else %>
+                {@room.topic}
+              <% end %>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layouts.app>
     """
   end
 end
